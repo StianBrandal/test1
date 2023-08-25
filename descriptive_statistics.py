59cc6d7656470bc14e776524d6591143d5e2dedf
StianBrandal <Stianbrandal@tussa.com>
2023-08-25 10:29:11 +0200

    fourth commit

---
+++ descriptive_statistics.py
@@ -0,0 +1,68 @@
+
+import numpy as np
+import pandas as pd
+import statistics
+import matplotlib.pyplot as plt
+
+#https://www.kaggle.com/code/hamelg/python-for-data-21-descriptive-statistics/notebook
+
+spring = pd.read_excel(r"C:/Users/stian/OneDrive/Documents/Skule/ADA501/Øvinger/Øving 2/spring.xlsx")
+
+svalbard_met = pd.read_csv(r"C:/Users/stian/OneDrive/Documents/Skule/ADA501/Øvinger/Øving 2/svalbard_met.csv")
+
+
+
+print(spring)
+print(spring["x"])
+
+
+mean1 = spring["x"].mean()
+print(mean1)
+
+standard_dev = statistics.stdev(spring["x"])
+print(standard_dev)
+
+covMat = np.stack((spring["x"],spring["y"]),axis=0) # getting the x and y row from the excel file because if not when using np.cov on spring you get the first colum as well.
+print(np.cov(covMat))
+
+
+print()
+print(covMat)
+corr = np.corrcoef(covMat)
+print(corr)
+
+
+
+print("\n\n\n\n\n")
+
+print(covMat[0])
+
+xAxis =covMat[0] # [1,3,5,7] 
+yAxis = covMat[1] #[2,4,6,1] #
+# plt.plot(xAxis,yAxis)
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+
+# plt.title("A simple graph")
+# #plt.show()
+
+# #  Regression Lines,
+# plt.plot(xAxis,yAxis,"o")
+# plt.xlabel("X-axis2")
+# plt.ylabel("Y-axis2")
+
+# m, b = np.polyfit(xAxis,yAxis,1)
+# plt.plot(xAxis,m*xAxis+b)
+# plt.title("A simple graph2")
+#plt.show()
+
+# scatterplots 
+plt.scatter(xAxis,yAxis)
+plt.show()
+
+
+
+plt.hist()
+
+
+#print(svalbard_met)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import math
-import numpy as np 
+#import math
+#import numpy as np 
 
 def asc_des_none(lst, s):
 
@@ -13,4 +13,96 @@ def asc_des_none(lst, s):
         return lst
 
 
-print(asc_des_none([1,3,2,4],"asc"))
+#print(asc_des_none([1,3,2,4],"asc"))
+
+
+
+def V_DAC(value):
+    DAC =(value/1023)*5
+    DACround = round(DAC,2)
+    return DACround
+
+#print(V_DAC(1000))
+
+
+
+def factorial(intereger):
+    total =1
+    for i in range(1,intereger+1):
+        total = i*total
+
+    return total
+
+
+
+#print(factorial(13))
+
+
+def billableDay(days):
+
+    if(days<=32):
+        return 0
+    elif(days >=33 and days <=40):
+        tempVal = days - 32
+        outpay = tempVal *325
+        return outpay
+    elif(days >= 41 and days <= 48):
+        tempVal= days -32
+        outpay = (8*325) + tempVal*550
+        return outpay
+    elif(days >= 48):
+        tempVal = days - 48
+        outpay = (8*325) + 8*550 + tempVal*600
+        return outpay
+
+
+#print(billableDay(50))
+
+
+def dis(cost,prosent):
+
+    sale =(cost*prosent)/100
+    total = cost-sale
+    return round(total,2)
+
+
+#print(dis(100,75))
+
+
+
+def fizz_buzz(number):
+    if(number%3==0 and number%5==0):
+        return "FizzBuzz"
+    elif (number%3==0):
+        return "Fizz"
+    elif(number%5 == 0 ):
+        return "Buzz"
+    else:
+        return1 = str(number)
+        return return1
+    
+
+
+
+#print(fizz_buzz(4))
+
+
+
+def fib_fast(numb):
+    a = 0
+    b = 1
+
+
+    for i in range(numb+1):
+        if i <= 1:
+            print(i)
+        else: 
+            c = a + b
+            a = b
+            b = c
+            print(c)
+
+    return c
+
+
+print(fib_fast(10))
